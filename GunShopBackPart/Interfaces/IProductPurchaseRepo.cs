32a835e4266ed2bd85bcdc6a65b2ee2ba2f85c22
Saenko47 <aleksejneizvestnyj39@gmail.com>
2026-04-27 20:30:33 +0300

    Added purchase

---
+++ GunShopBackPart/Interfaces/IProductPurchaseRepo.cs
@@ -0,0 +1,12 @@
+﻿using GunShopBackPart.DTOs;
+using GunShopBackPart.Models;
+
+namespace GunShopBackPart.Interfaces
+{
+    public interface IProductPurchaseRepo
+    {
+        Task<List<ProductPurchase>?> GetAllPurchaseOfCustomerAsync(int customerId);
+
+        Task Purchase(int customerId, int productId);
+    }
+}
---
+++ GunShopBackPart/Repository/ProductPurchaseRepo.cs
@@ -0,0 +1,59 @@
+﻿using GunShopBackPart.Data;
+using GunShopBackPart.DTOs;
+using GunShopBackPart.Interfaces;
+using GunShopBackPart.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GunShopBackPart.Repository
+{
+    public class ProductPurchaseRepo: IProductPurchaseRepo
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly DbSet<ProductPurchase> set;
+
+        public ProductPurchaseRepo(ApplicationDBContext context)
+        {
+            _context = context;
+            set = context.Set<ProductPurchase>();
+        }
+
+        public async Task<List<ProductPurchase>?> GetAllPurchaseOfCustomerAsync(int customerId)
+        {
+            var purchases = await set.Where(p => p.CustomerId == customerId).ToListAsync();
+            if(purchases == null)
+            {
+                throw new Exception("No purchases found for the customer.");
+            }
+            return purchases;
+        }
+        private async Task<InventoryItem> FindProductById(int productId) 
+        {
+            var product = await _context.Set<InventoryItem>()
+       .FirstOrDefaultAsync(p => p.Id == productId);
+
+            if (product == null)
+            {
+                throw new KeyNotFoundException("Product not found.");
+            }
+            return product;
+        }
+        public async Task Purchase(int customerId, int productId)
+        {
+            var inventoryItem = await FindProductById(productId);
+
+            ProductPurchase purchase = new ProductPurchase
+            {
+                CustomerId = customerId,
+                InventoryItemId = inventoryItem.Id,
+                PurchaseDate = DateTime.Now 
+            };
+            set.Add(purchase);
+
+            await _context.SaveChangesAsync();
+           
+        }
+    }
+}
