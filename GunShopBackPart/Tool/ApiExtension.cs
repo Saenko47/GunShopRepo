3ebc4a1efdcf24f0686e406ea36b67ffa365985d
Saenko47 <aleksejneizvestnyj39@gmail.com>
2026-04-29 00:15:11 +0300

    JWT small addition

---
+++ GunShopBackPart/Tool/ApiExtension.cs
@@ -0,0 +1,28 @@
+﻿using GunShopBackPart.Tool.JVT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using System.Runtime.CompilerServices;
+
+namespace GunShopBackPart.Tool
+{
+    public static class ApiExtension
+    {
+        public static void AddAuthenticationHeader(this IServiceCollection services
+            , IOptions<JvtOptions> options)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+                AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+                {
+                    opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+                            System.Text.Encoding.UTF8.GetBytes(options.Value.SecretKey)),
+                        ValidateLifetime = true
+                    };
+                });
+            services.AddAuthorization();
+        }
+    }
+}
